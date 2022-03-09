753881db90970dd8c7f91dd75522754760c76fc3
João Vitor <carryonlink@gmail.com>
2022-03-09 14:43:51 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,29 @@
+import { ReactComponent as Githubicon } from 'assets/img/Github.svg';
+import './style.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/JVFNeto">
+                        <div className="dsmovie-contact-container">
+                            <Githubicon />
+                            <p className='dsmovie-contact-link'>/JVFNeto</p>
+                        </div>
+
+
+                    </a>
+                </div>
+
+
+            </nav>
+
+
+        </header>
+    );
+
+}
+
+export default Navbar;

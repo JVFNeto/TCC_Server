e937fb88a434b18cf7ed558ffd9cdbe49884fcb6
João Vitor <carryonlink@gmail.com>
2022-03-11 00:55:18 -0300

    _redirects

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
+                    <h1>Movies</h1>
                     <a href="https://github.com/JVFNeto">
                         <div className="dsmovie-contact-container">
                             <Githubicon />

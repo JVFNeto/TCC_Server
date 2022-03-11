ec4021ace776e771a15e0125efb0dbffa9d0ca80
João Vitor <carryonlink@gmail.com>
2022-03-11 14:46:35 -0300

    Voltei um commit atrás

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,13 +1,15 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
-import { Movie } from "types/movie";
 
-type Props ={
-    movie: Movie;
-}
+function MovieCard() {
 
-function MovieCard({ movie }: Props) {
-
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
         <div>
@@ -15,7 +17,6 @@ function MovieCard({ movie }: Props) {
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
 

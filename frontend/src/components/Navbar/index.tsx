c1e0809837378b7525ef784a155d2d42e9820507
João Vitor <carryonlink@gmail.com>
2022-03-12 13:06:57 -0300

    Finalizado o Frontend

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,11 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <h1>Movies</h1>
+                    <a href="https://github.com/JVFNeto">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/Perfil do Desenvolvedor</p>
                         </div>
                     </a>
                 </div>
